812e54165ee0fa9b2a0bd4d9f1efe28bad44751d
shanemachine <szackery1244@gmail.com>
2017-03-16 08:48:28 +0000

    add var for url

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
     
-mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://Shane:password@ds131900.mlab.com:31900/camp");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v4";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
