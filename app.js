06135583a39900e3dca4418f98003617605ca101
shanemachine <szackery1244@gmail.com>
2017-03-16 08:36:42 +0000

    update databse

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
     
-// mongoose.connect("mongodb://localhost/yelp_camp_v4");
-mongoose.connect("mongodb://Shane:password@ds131900.mlab.com:31900/camp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://Shane:password@ds131900.mlab.com:31900/camp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
